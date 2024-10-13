61b273bee982571971958a3e5e6e4a8990caa93d
AMAIISDOM\alahmer <ahmed.lahmer@amadeus.com>
2024-10-13 16:03:34 +0200

    add s3 bucket

---
+++ infra.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-tf-test-bucket"
+  tags = {
+    Name        = "test_bucket"
+    Environment = "Dev"
+  }
+}
